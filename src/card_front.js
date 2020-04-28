e274aa01f7a3662de219f83b7097487d8167d78a
haejun <haejun.chung210@gmail.com>
2020-04-28 15:45:35 -0400

    Branching

---
+++ src/card_front.js
@@ -0,0 +1,6 @@
+const cardFront = document.getElementsByClassName("card-front");
+
+function createCardFront () {
+    cardFront.createElement("div");
+    cardFront.createClassList("card-front-content")
+}
